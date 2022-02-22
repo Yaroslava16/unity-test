49fe81fc11bfc4082a00afc914e83a16d961f8c5
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2022-02-22 11:47:15 +0200

    start making a sidebar

---
+++ components/sideBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="side-bar">
+    <div class="side-bar__upper-block">
+      <n-link :to="'/'"
+        ><img class="side-bar__logo" src="~/assets/svg/logo.svg" alt="" />
+      </n-link>
+      <button class="side-bar__btn" type="button"><img :src="btn.img" alt=""></button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    btn: {},
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.side-bar {
+  padding: 3.889vw 6.667vw 3.889vw 3.889vw;
+  &__upper-block {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 9.722vw;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <template>
- 
+  <SideBar :btn="btn" />
 </template>
 
 <script>
+import SideBar from "~/components/sideBar.vue";
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+  components: {
+    SideBar,
+  },
+  data() {
+    return {
+      btn: {
+        img: require("~/assets/svg/cross.svg"),
+      },
+    };
+  },
+};
 </script>
